b744ad44058a50e1f3d91d452a7910d0a12989d1
Anuranjan Gyawali <anuranjangyawali930@proton.me>
2026-05-04 16:18:14 +0545

    use hashes of UUID objects instead of UUID strings

--- filehost.py
+++ filehost.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from typing import Annotated, IO
-from fastapi import FastAPI, File, Header, UploadFile, HTTPException
+from fastapi import FastAPI, File, Header, UploadFile, HTTPException, Form
+from pydantic import BaseModel
 from uuid import uuid4
 from fastapi.responses import FileResponse
 from tempfile import NamedTemporaryFile
@@ -17,21 +18,21 @@ def root():
 
 @app.post("/upload")
 def upload_file(uploaded_file: UploadFile, content_length: Annotated[int | None, Header(lt=6*1024*1024)]):
-    UUID=str(uuid4())
     total_size_limit = 6 * 1024 *1024
     chunk_size = 0
+    file_id = hash(uuid4())
 
     for chunk in uploaded_file.file:
         chunk_size += len(chunk)
         if chunk_size > total_size_limit:
             return { "Info": "Uploaded file is too large. Expected file size less than 6MiB"}
 
-    with open(f"{DIR}/{UUID}", "wb") as wfile:
+    with open(f"{DIR}/{file_id}", "wb") as wfile:
         content = uploaded_file.file.read()
         wfile.write(content)
-    return {"URL": f"Uploaded file can be accessed at /download/{UUID}" }
+    return {"URL": f"Uploaded file can be accessed at /download/{file_id}" }
 
-@app.get("/download/{UUID}")
-def download_file(UUID: str):
-    return FileResponse(f"{DIR}/{UUID}")
+@app.get("/download/{file_id}")
+def download_file(file_id: str):
+    return FileResponse(f"{DIR}/{file_id}")
 
