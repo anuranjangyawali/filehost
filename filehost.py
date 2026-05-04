8afe2c062c7a3d6843a29b94b155edfc15d484d1
Anuranjan Gyawali <anuranjangyawali930@proton.me>
2026-05-04 21:16:06 +0545

    improve file size validation & shorten file IDs

--- filehost.py
+++ filehost.py
@@ -17,22 +17,25 @@ def root():
     return { "Message": "This is the API for my filehost."}
 
 @app.post("/upload")
-def upload_file(uploaded_file: UploadFile, content_length: Annotated[int | None, Header(lt=6*1024*1024)]):
-    total_size_limit = 6 * 1024 *1024
-    chunk_size = 0
-    file_id = hash(uuid4())
+def upload_file(uploaded_file: Annotated[bytes, File()]):
+    size_limit = 6 * 1024 *1024
+    read_bytes = 0
 
-    for chunk in uploaded_file.file:
-        chunk_size += len(chunk)
-        if chunk_size > total_size_limit:
-            return { "Info": "Uploaded file is too large. Expected file size less than 6MiB"}
+    UUID = str(uuid4())
+    split_UUID = UUID.split('-')
+    ID = split_UUID[0]
 
-    with open(f"{DIR}/{file_id}", "wb") as wfile:
-        content = uploaded_file.file.read()
-        wfile.write(content)
-    return {"URL": f"Uploaded file can be accessed at /download/{file_id}" }
+    for file_bytes in uploaded_file:
+        read_bytes += file_bytes
+        if read_bytes > size_limit:
+            return { "Info": "Uploaded file is too large. Expected file size less than 6MiB" }
 
-@app.get("/download/{file_id}")
-def download_file(file_id: str):
-    return FileResponse(f"{DIR}/{file_id}")
+    with open(f"{DIR}/{ID}", "wb") as local_file:
+        local_file.write(uploaded_file)
+
+    return {"URL": f"Uploaded file can be accessed at http://127.0.0.1:8000/download/{ID}" }
+
+@app.get("/download/{ID}")
+def download_file(ID: str):
+    return FileResponse(f"{DIR}/{ID}")
 
