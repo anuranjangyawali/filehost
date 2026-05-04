e395ecbf02112e73b5f69dd5a96cfc67d34bdf7b
Anuranjan Gyawali <anuranjangyawali930@proton.me>
2026-05-04 15:02:01 +0545

    add validation to check file size doesn't exceed 6MiB

--- filehost.py
+++ filehost.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
 
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
-from typing import Annotated
+from typing import Annotated, IO
+from fastapi import FastAPI, File, Header, UploadFile, HTTPException
 from uuid import uuid4
+from fastapi.responses import FileResponse
+from tempfile import NamedTemporaryFile
+from starlette import status
 
 app = FastAPI()
 
 DIR="storage"
-UUID=str(uuid4())
 
 @app.get("/")
-async def root():
+def root():
     return { "Message": "This is the API for my filehost."}
 
 @app.post("/upload")
-async def upload_file(uploaded_file: UploadFile):
+def upload_file(uploaded_file: UploadFile, content_length: Annotated[int | None, Header(lt=6*1024*1024)]):
+    UUID=str(uuid4())
+    total_size_limit = 6 * 1024 *1024
+    chunk_size = 0
+
+    for chunk in uploaded_file.file:
+        chunk_size += len(chunk)
+        if chunk_size > total_size_limit:
+            return { "Info": "Uploaded file is too large. Expected file size less than 6MiB"}
+
     with open(f"{DIR}/{UUID}", "wb") as wfile:
-        content = await uploaded_file.read()
+        content = uploaded_file.file.read()
         wfile.write(content)
-    return {"URL": f"{uploaded_file.filename} can be accessed at /download/{UUID}" }
+    return {"URL": f"Uploaded file can be accessed at /download/{UUID}" }
 
 @app.get("/download/{UUID}")
-async def download_file(UUID: str):
+def download_file(UUID: str):
     return FileResponse(f"{DIR}/{UUID}")
-    
-
-
-
 
