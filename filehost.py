5043044c7a859437488aafc719b8ffc0a19d6b9d
Anuranjan Gyawali <anuranjangyawali930@proton.me>
2026-05-05 14:28:30 +0545

    add functionality to upload multiple files & restructure code

--- filehost.py
+++ filehost.py
@@ -1,39 +1,57 @@
 #!/usr/bin/env python
 
-from typing import Annotated, IO
-from fastapi import FastAPI, File, Header, UploadFile, HTTPException, Form
-from pydantic import BaseModel
-from uuid import uuid4
+import os
+from typing import Annotated
+from fastapi import FastAPI, File
 from fastapi.responses import FileResponse
-from tempfile import NamedTemporaryFile
-from starlette import status
+from uuid import uuid4
 
 app = FastAPI()
 
 DIR="storage"
 
+def size_check(files, file_no):
+    size_limit = 6291456
+    read_bytes = 0
+
+    for file_bytes in files[file_no]:
+        read_bytes += file_bytes
+        if read_bytes > size_limit:
+            return f"Uploaded file no {file_no} is too large."
+        else:
+            return files[file_no]
+
+def item_id():
+    UUID = str(uuid4())
+    split_UUID = UUID.split('-')
+    ID = split_UUID[0]
+    return ID
+
 @app.get("/")
 def root():
     return { "Message": "This is the API for my filehost."}
 
+@app.get("/list")
+def list_files():
+    list_of_files = os.listdir(DIR)
+    return list_of_files
+
 @app.post("/upload")
-def upload_file(uploaded_file: Annotated[bytes, File()]):
-    size_limit = 6 * 1024 *1024
-    read_bytes = 0
+def upload_file(files: Annotated[list[bytes], File(title="Upload File", description="Upload file size less than 6MiB")]):
+    ID = item_id()
+    response = []
+    for file_no in range(len(files)):
+        file = size_check(files, file_no)
 
-    UUID = str(uuid4())
-    split_UUID = UUID.split('-')
-    ID = split_UUID[0]
+        if type(file) is str:
+            response.append(file)
+        else:
+            with open(f"{DIR}/{ID}", "wb") as local_file:
+                local_file.write(files[file_no])
+                response.append(f"Uploaded file no {file_no} can be accessed at http://127.0.0.1:8000/download/{ID}")
 
-    for file_bytes in uploaded_file:
-        read_bytes += file_bytes
-        if read_bytes > size_limit:
-            return { "Info": "Uploaded file is too large. Expected file size less than 6MiB" }
+    return { "URLs": response, "No of files uploaded:": len(files)}
 
-    with open(f"{DIR}/{ID}", "wb") as local_file:
-        local_file.write(uploaded_file)
-
-    return {"URL": f"Uploaded file can be accessed at http://127.0.0.1:8000/download/{ID}" }
 
 @app.get("/download/{ID}")
 def download_file(ID: str):
